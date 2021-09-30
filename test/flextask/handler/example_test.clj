a601cf278fd199b0b8c1d89a4467c6e71764ed4f
Ion Simion <ion@simion.dev>
2021-10-01 02:07:08 +0300

    Added project tests

--- test/flextask/handler/example_test.clj
+++ test/flextask/handler/example_test.clj
@@ -1,11 +1,30 @@
 (ns flextask.handler.example-test
-  (:require [clojure.test :refer :all]
-            [integrant.core :as ig]
-            [ring.mock.request :as mock]
-            [flextask.handler.example :as example]))
+    (:require [clojure.test :refer :all]
+      [integrant.core :as ig]
+      [ring.mock.request :as mock]
+      [flextask.handler.index :as index]
+      [flextask.handler.api.scramble :as scramble]
+      ))
 
-(deftest smoke-test
-  (testing "example page exists"
-    (let [handler  (ig/init-key :flextask.handler/example {})
-          response (handler (mock/request :get "/"))]
-      (is (= :ataraxy.response/ok (first response)) "response ok"))))
+(deftest index-test
+         (testing "Index page exists"
+                  (let [handler (ig/init-key :flextask.handler/index {})
+                        response (handler (mock/request :get "/"))]
+                       (is (= :ataraxy.response/ok (first response)) "response ok"))))
+
+(deftest scramble1-test
+         (testing "Scramble Api Test 1"
+                  (is (= true (scramble/scrambled? "rekqodlw" "world")) "rekqodlw VS world")))
+
+(deftest scramble2-test
+         (testing "Scramble Api Test 2"
+                  (is (= true (scramble/scrambled? "cedewaraaossoqqyt" "codewars")) "cedewaraaossoqqyt VS codewars")))
+
+(deftest scramble3-test
+         (testing "Scramble Api Test 3"
+                  (is (= false (scramble/scrambled? "katas" "steak")) "katas VS steak")))
+
+(deftest scramble4-test
+         (testing "Scramble Api Test 4"
+                  (is (= true (scramble/scrambled? "stressed" "desserts")) "stressed VS desserts")))
+
